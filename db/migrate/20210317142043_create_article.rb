9017f11ae2e6ac2a3cfacb48b6b31007b9dfd219
Sorawichaya Tiratrakoonwichaya <tsorawichaya@gmail.com>
2021-03-18 13:54:37 +0700

    edit create_article.rb

--- db/migrate/20210317142043_create_article.rb
+++ db/migrate/20210317142043_create_article.rb
@@ -5,14 +5,5 @@ class CreateArticle < ActiveRecord::Migration[6.1]
       t.string :body
       t.timestamps
     end
-
-    
-    change_table :articles do |t|
-      t.string :title
-      t.string :body
-      t.integer :stock
-      t.integer :price
-      t.timestamps
-    end
   end
 end
